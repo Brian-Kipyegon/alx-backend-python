7c3548d811a56691f9905ab4f30e4e66ab0f35a5
Brian-Kipyegon <kipyegon.c.brian@gmail.com>
2025-06-15 22:04:42 +0300

    Django-Chat added

---
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -0,0 +1,26 @@
+from django.db.models.signals import pre_save, post_delete
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import Message, MessageHistory, Notification
+
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if instance.pk:
+        try:
+            old = Message.objects.get(pk=instance.pk)
+            if old.content != instance.content:
+                MessageHistory.objects.create(
+                    message=instance, old_content=old.content, edited_by=instance.sender
+                )
+                instance.edited = True
+        except Message.DoesNotExist:
+            pass
+
+
+@receiver(post_delete, sender=User)
+def delete_user_related_data(sender, instance, **kwargs):
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+    Notification.objects.filter(user=instance).delete()
+    MessageHistory.objects.filter(edited_by=instance).delete()
